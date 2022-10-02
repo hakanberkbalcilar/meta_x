9d40a46b4cf36ac77583579d7f2ee74dd057af20
Hakan Berk Balcılar <78869152+hakanberkbalcilar@users.noreply.github.com>
2022-10-02 22:55:41 +0300

    Improve Code

--- android/src/main/kotlin/com/hbb/meta_x/MetadataProvider.kt
+++ android/src/main/kotlin/com/hbb/meta_x/MetadataProvider.kt
@@ -15,8 +15,9 @@ class MetadataProvider : MediaMetadataRetriever() {
     val metadata: HashMap<String, Any?>?
         get() {
             val metadata = HashMap<String, Any?>()
-
             val fileInfo = HashMap<String, Any?>()
+            metadata["fileInfo"] = fileInfo
+
             val splitPath = path.split("/")
             val splitFileName = splitPath.last().split(".")
 
@@ -25,55 +26,28 @@ class MetadataProvider : MediaMetadataRetriever() {
             fileInfo["path"] = path
             fileInfo["directory"] = splitPath[splitPath.size - 2]
 
-            metadata["fileInfo"] = fileInfo
+            metadata["title"] = getMetaData(METADATA_KEY_TITLE)
+            metadata["artists"] = getMetaData(METADATA_KEY_ARTIST)
+            metadata["album"] = getMetaData(METADATA_KEY_ALBUM)
+            metadata["albumArtist"] = getMetaData(METADATA_KEY_ALBUMARTIST)
+            metadata["genre"] = getMetaData(METADATA_KEY_GENRE)
+            metadata["author"] = getMetaData(METADATA_KEY_AUTHOR)
+            metadata["writer"] = getMetaData(METADATA_KEY_WRITER)
+            metadata["discNumber"] = getMetaData(METADATA_KEY_DISC_NUMBER)
+            metadata["mimeType"] = getMetaData(METADATA_KEY_MIMETYPE)
+            metadata["duration"] = getMetaData(METADATA_KEY_DURATION)
+            metadata["bitrate"] = getMetaData(METADATA_KEY_BITRATE)
+            metadata["trackNumber"] = getMetaData(METADATA_KEY_CD_TRACK_NUMBER)
+            metadata["year"] = getMetaData(METADATA_KEY_YEAR)
 
             try {
-                metadata["title"] = getMetaData(METADATA_KEY_TITLE)
-                metadata["artists"] = getMetaData(METADATA_KEY_ARTIST)
-                metadata["album"] = getMetaData(METADATA_KEY_ALBUM)
-                metadata["albumArtist"] = getMetaData(METADATA_KEY_ALBUMARTIST)
-                metadata["genre"] = getMetaData(METADATA_KEY_GENRE)
-                metadata["author"] = getMetaData(METADATA_KEY_AUTHOR)
-                metadata["writer"] = getMetaData(METADATA_KEY_WRITER)
-                metadata["discNumber"] = getMetaData(METADATA_KEY_DISC_NUMBER)
-                metadata["mimeType"] = getMetaData(METADATA_KEY_MIMETYPE)
-                metadata["duration"] = getMetaData(METADATA_KEY_DURATION)
-                metadata["bitrate"] = getMetaData(METADATA_KEY_BITRATE)
                 metadata["albumArt"] = embeddedPicture
-
-                val trackNumber = getMetaData(METADATA_KEY_CD_TRACK_NUMBER)
-                try {
-
-                    if (trackNumber == null) {
-                        metadata["trackNumber"] = null
-                        metadata["albumLength"] = null
-                    } else {
-                        val splitTrackNumber = trackNumber.split("/".toRegex()).toTypedArray()
-                        metadata["trackNumber"] = splitTrackNumber[0].trim { it <= ' ' }
-                        metadata["albumLength"] = splitTrackNumber[splitTrackNumber.size - 1].trim { it <= ' ' }
-                    }
-                } catch (error: Exception) {
-                    metadata["trackNumber"] = null
-                    metadata["albumLength"] = null
-                }
-                val year = getMetaData(METADATA_KEY_YEAR)
-                val date = getMetaData(METADATA_KEY_DATE)
-
-                if (year == null)
-                    if (date == null)
-                        metadata["year"] = null
-                    else
-                        metadata["year"] =
-                            date.split("-".toRegex()).toTypedArray()[0].trim { it <= ' ' }
-                else
-                    metadata["year"] = year.trim { it <= ' ' }.toInt()
-
-                return metadata
             }
             catch (e:IllegalStateException){
                 Log.e("MetaXError", e.message ?: "Unknown")
-                return metadata
             }
+            return metadata
+
         }
 
         private fun getMetaData(i:Int): String?{
